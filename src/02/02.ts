78e9141664f1d789a8f472c782ba821695f42302
sk3llytoon <hatsz141415@gmail.com>
2022-05-30 15:24:18 +0300

    2-nd lesson

---
+++ src/02/02.ts
@@ -0,0 +1,17 @@
+const student = {
+    name: "Gera",
+    age: 21,
+    isActive: true,
+    adress: {
+        streetTitle: "Surganova",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+
+    ]
+}
+
+console.log(student.adress.city.title)
