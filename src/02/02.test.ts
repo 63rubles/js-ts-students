d06860b100c5254516cdcbe99c32fa6dba52ebfe
sk3llytoon <hatsz141415@gmail.com>
2022-06-08 20:26:12 +0300

    js/ts for students 2 --done

--- src/02/02.test.ts
+++ src/02/02.test.ts
@@ -5,8 +5,35 @@ let city: CityType;
 beforeEach(() => {
     city = {
         title: "New York",
-        houses: [{},{},{}],
-        governmentBuildings: [{},{}],
+        houses: [
+            {
+                buildedAt: 2012,
+                repaired: false,
+                address: {number: 100, street: {title: "White street"}}
+            },
+            {
+                buildedAt: 2008,
+                repaired: false,
+                address: {number: 100, street: {title: "Happy street"}}
+            },
+            {
+                buildedAt: 2020,
+                repaired: false,
+                address: {number: 101, street: {title: "Happy street"}}
+            }
+        ],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL",
+                budget: 200000,
+                staffCount: 200, address: {street: {title: "Central Str"}}
+            },
+            {
+                type: "FIRE-STATION",
+                budget: 500000,
+                staffCount: 1000, address: {street: {title: "South Str"}}
+            }
+        ],
         citizensNumber: 1000000
     }
 })
@@ -34,7 +61,7 @@ test("test city should contains 3 houses", () => {
 
 // 01. дополните тип GovernmentBuildingType
 // 02. заполните объект city, чтобы тесты ниже прошли
-test.skip("test city should contains hospital and fire station", () => {
+test("test city should contains hospital and fire station", () => {
     expect(city.governmentBuildings.length).toBe(2);
 
     expect(city.governmentBuildings[0].type).toBe("HOSPITAL");
--- src/02/02_02.ts
+++ src/02/02_02.ts
@@ -1,21 +1,36 @@
+export type LocalSreetType = {
+    title: string
+}
+
+export type LocalAdressType = {
+    number: number
+    street: LocalSreetType
+}
+
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: LocalAdressType
+}
+
+export type GovernmentStreetType = {
+    title: string
+}
+
+export type GovernmentAddressType = {
+    street: GovernmentStreetType
+}
+
+export type GovernmentBuildingType = {
+    type: string
+    budget: number
+    staffCount: number
+    address: GovernmentAddressType
+}
+
 export type CityType = {
     title: string
     houses: Array<HouseType>
-    governmentBuildings: Array<string>
+    governmentBuildings: Array<GovernmentBuildingType>
     citizensNumber: number
-}
-
-type HouseType = {
-    buildedAt: number
-    repaired: boolean
-    address: Array<LocalAdressType>
-}
-
-type LocalAdressType = {
-    number: number
-    street: Array<LocalSreetType>
-}
-
-type LocalSreetType = {
-    title: string
 }
