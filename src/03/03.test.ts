9557a65b49aa88a756584f585d5d6f4ac622f450
sk3llytoon <hatsz141415@gmail.com>
2022-06-10 14:33:04 +0300

    js/ts for students 3 --function addSkill added

---
+++ src/03/03.test.ts
@@ -0,0 +1,41 @@
+import {StudentType} from "../02/02";
+import {addSkill} from "./03_03";
+
+let student: StudentType;
+beforeEach(() => {
+    student = {
+        id: 1,
+        name: "Gera",
+        age: 21,
+        isActive: true,
+        adress: {
+            streetTitle: "Surganova",
+            city: {
+                title: "Minsk",
+                countryTitle: "Belarus"
+            }
+        },
+        technologies: [
+            {
+                id: 1,
+                title: "React"
+            },
+            {
+                id: 2,
+                title: "Redux"
+            },
+            {
+                id: 3,
+                title: "TS"
+            }
+        ]
+    }
+})
+
+test("new tech skill should be added to student", ()=>{
+    expect(student.technologies.length).toBe(3)
+    addSkill(student, "JS")
+    expect(student.technologies.length).toBe(4)
+    expect(student.technologies[3].title).toBe("JS")
+    expect(student.technologies[3].id).toBeDefined()
+})
---
+++ src/03/03_03.ts
@@ -0,0 +1,8 @@
+import {student, StudentType} from "../02/02";
+
+export const addSkill = (student: StudentType, skill: string) => {
+    student.technologies.push({
+        id: new Date().getTime(),
+        title: skill
+    })
+}
